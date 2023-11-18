c6d7ad4fcac60d7861c149879f0c824d67c9398f
Safo Emmanuel <nanasafo122@gmail.com>
2023-11-18 12:13:49 +0000

    nineth

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,13 @@
+const CourseIdPage = ({
+    params
+}: {
+    params: {courseId: string}
+}) => {
+    return ( 
+        <div>
+            COurse Id
+        </div>
+     );
+}
+ 
+export default CourseIdPage;
--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -29,8 +29,9 @@ const CreatePage = () => {
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
-            const response = await axios.post("/api/course", values)
-            router.push(`/teacher/courses/$(response.data.id)`)
+            const response = await axios.post("/api/courses", values)
+            router.push(`/teacher/courses/${response.data.id}`);
+            toast.success("Course Created")
         } catch  {
             toast.error("Something Went Wrong")
         }
